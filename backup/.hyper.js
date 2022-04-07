28588185330e2de3a1eb31a9403dbe171822db93
Luiz <luizfranzon05@hotmail.com>
2022-04-07 01:24:15 -0300

    Auto stash before merge of "main" and "origin/main"
    reorganization; more configs; terminal icon;

--- backup/.hyper.js
+++ backup/.hyper.js
@@ -4,13 +4,17 @@
 // See https://hyper.is#cfg for all currently supported options.
 module.exports = {
     config: {
+        hyperBorder: {
+            borderColors: ['#6514f5','#33d0b9', '#6514f5'],
+            borderWidth: '6px'
+        },
         // choose either `'stable'` for receiving highly polished,
         // or `'canary'` for less polished but more frequent updates
         updateChannel: 'stable',
         // default font size in pixels for all tabs
         fontSize: 18,
         // font family with optional fallbacks
-        fontFamily: '"Fira Code Retina", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+        fontFamily: '"Fira Code", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
         // default font weight: 'normal' or 'bold'
         fontWeight: 'normal',
         // font weight for bold characters: 'normal' or 'bold'
@@ -92,10 +96,11 @@ module.exports = {
         //
         // Cygwin
         // - Example: `C:\\cygwin64\\bin\\bash.exe`
-
         shell: 'C:\\Windows\\System32\\wsl.exe',
+        // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
+        // by default `['--login']` will be used
+        shellArgs: ['~'],
         
-        shellArgs: [],
         // for environment variables
         env: {},
         // Supported Options:
@@ -137,7 +142,7 @@ module.exports = {
     //   `hyperpower`
     //   `@company/project`
     //   `project#1.0.1`
-    plugins: ["hyper-dracula"],
+    plugins: ['hyper-omni-theme', 'hyperborder'],
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
     // to load it and avoid it being `npm install`ed
